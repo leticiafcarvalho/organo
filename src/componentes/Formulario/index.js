de4e641ab682437fadacd6f810bcd747e0637f7a
leticiafcarvalho <leticiacarvalhos2@outlook.com>
2024-11-04 13:53:56 -0300

    form submit

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,9 +1,10 @@
-import Botao from '../Botao';
-import CampoTexto from '../CampoTexto';
-import ListaSuspensa from '../ListaSuspensa';
-import './Formulario.css';
+import { useState } from 'react'
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -13,27 +14,60 @@ const Formulario = () => {
         'UX e Design',
         'Mobile',
         'Inovação e Gestão'
-        ]
+    ]
 
-    const aoSalvar = (evento) => { 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido')
-    }    
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+    }
 
     return (
-        <section className='formulario'>
+        <section className="formulario">
             <form onSubmit={aoSalvar}>
-            <h2>Preencha os dados para criar o card do colaborador:</h2>
-            <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
-            <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
-            <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-            <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto 
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome" 
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo" 
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem" 
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time"
+                    itens={times}
+                    valor={time}
+                    aoAlterar={valor => setTime(valor)}
+                />
                 <Botao>
-                    Criar card
+                    Criar Card
                 </Botao>
             </form>
         </section>
     )
 }
 
-export default Formulario;
+export default Formulario
