014ccd01bf60bf0fa287691e0eb00964b21c1bfd
leticiafcarvalho <leticiacarvalhos2@outlook.com>
2024-11-04 14:36:04 -0300

    atualizacao time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
@@ -13,8 +14,11 @@ function App() {
 
   return (
     <div className="App">
-      <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+        <Banner /> 
+        <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/> 
+        <Time nome="Programação"/>
+        <Time nome="Front-End"/>
+        <Time nome="Data Science"/>
     </div>
   );
 }
