257ad29aa9935f5830ceb203ad4a589d4f33387a
leticiafcarvalho <leticiacarvalhos2@outlook.com>
2024-11-04 15:28:25 -0300

    times.map

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,16 +6,6 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
-
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
@@ -58,9 +48,9 @@ const Formulario = (props) => {
                 <ListaSuspensa
                     obrigatorio={true}
                     label="Time"
-                    itens={times}
+                    itens={props.times}
                     valor={time}
-                    aoAlterar={valor => setTime(valor)}
+                    aoAlterado={valor => setTime(valor)}
                 />
                 <Botao>
                     Criar Card
